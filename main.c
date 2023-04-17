aba7c1f486384acb8b0063d0f096dba579f221be
mosesgitonga <infosec947@gmail.com>
2023-04-16 23:16:11 -0700

    shell prompt display and exec

---
+++ main.c
@@ -0,0 +1,7 @@
+#include "main.h"
+
+int main(void)
+{
+	display();
+	return (0);
+}
---
+++ main.h
@@ -0,0 +1,15 @@
+#ifndef MAIN_H
+#define MAIN_H
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <unistd.h>
+#include <string.h>
+
+
+
+
+int display();
+#endif
+
