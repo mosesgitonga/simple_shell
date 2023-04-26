4e4543da614c6ae1922d48d3b393f0b67bd57c0f
mosesgitonga <infosec947@gmail.com>
2023-04-26 23:29:48 +0300

    more change

--- errors.c
+++ errors.c
@@ -1,85 +1,128 @@
 #include "shell.h"
 
 /**
- *_eputs - prints an input string
- * @str: the string to be printed
+ *_eputs -str
+ * @string: str 
+*
  *
- * Return: Nothing
+ * Return: VOID
  */
-void _eputs(char *str)
+void _eputs(char *string)
 {
 	int i = 0;
 
-	if (!str)
+
+
+	if (!string)
+		
+
 		return;
-	while (str[i] != '\0')
+
+
+	while (string[i] != '\0')
+
+
 	{
-		_eputchar(str[i]);
+		_eputchar(string[i]);
 		i++;
 	}
 }
 
 /**
- * _eputchar - writes the character c to stderr
- * @c: The character to print
+ * _eputchar - char c
+ * @ch:charactertoprint
  *
  * Return: On success 1.
  * On error, -1 is returned, and errno is set appropriately.
  */
-int _eputchar(char c)
+
+
+
+
+
+int _eputchar(char ch)
 {
 	static int i;
+
 	static char buf[WRITE_BUF_SIZE];
 
-	if (c == BUF_FLUSH || i >= WRITE_BUF_SIZE)
+
+
+
+
+
+	if (ch == BUF_FLUSH || i >= WRITE_BUF_SIZE)
 	{
+
+
+
+
 		write(2, buf, i);
 		i = 0;
+
 	}
-	if (c != BUF_FLUSH)
-		buf[i++] = c;
+	if (ch != BUF_FLUSH)
+
+		buf[i++] = ch;
 	return (1);
 }
 
 /**
- * _putfd - writes the character c to given fd
- * @c: The character to print
- * @fd: The filedescriptor to write to
+ * _putfd -writes to fd
+ * @ch: The character to print
+ * @fdes:filedescriptor
  *
- * Return: On success 1.
- * On error, -1 is returned, and errno is set appropriately.
+ * Return:1.
  */
-int _putfd(char c, int fd)
+int _putfd(char ch, int fdes)
 {
+
+
 	static int i;
 	static char buf[WRITE_BUF_SIZE];
 
-	if (c == BUF_FLUSH || i >= WRITE_BUF_SIZE)
+
+
+	if (ch == BUF_FLUSH || i >= WRITE_BUF_SIZE)
 	{
-		write(fd, buf, i);
+
+
+
+		write(fdes, buf, i);
 		i = 0;
 	}
-	if (c != BUF_FLUSH)
-		buf[i++] = c;
+
+	if (ch != BUF_FLUSH)
+
+
+		buf[i++] = ch;
+
 	return (1);
 }
 
 /**
- *_putsfd - prints an input string
- * @str: the string to be printed
- * @fd: the filedescriptor to write to
+ *_putsfd -input string
+ * @tri: str
+ * @fdes:filedescriptor
  *
- * Return: the number of chars put
+ * Return: 0;
+*
  */
-int _putsfd(char *str, int fd)
+int _putsfd(char *tri, int fdes)
 {
 	int i = 0;
 
-	if (!str)
+
+	if (!tri)
+
 		return (0);
-	while (*str)
+	while (*tri)
 	{
-		i += _putfd(*str++, fd);
+
+
+
+		i += _putfd(*tri++, fdes);
+
 	}
 	return (i);
 }
