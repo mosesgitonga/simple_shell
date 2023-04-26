23b1ec87e245d1ce0f693d641c858e1e004d9202
mosesgitonga <infosec947@gmail.com>
2023-04-26 08:31:26 -0700

    isks

--- main.c
+++ main.c
@@ -1,9 +1,60 @@
 #include <stdio.h>
+#include <string.h>
 #include <stdlib.h>
-#include "main.h"
+#include <unistd.h>
+#include <sys/wait.h>
 
-int main(int argc, char **argv, char **envp)
+
+
+
+
+
+#define PROMPT "#cisfun$ "
+
+void execute(char *commandd) 
 {
-    simple_shell(argv, envp);
-    return 0;
+    char *argv[] = {commandd, NULL};
+
+    if (execve(commandd, argv, NULL) == -1) 
+    {
+        perror("./shell: ");
+    }
 }
+
+int main(void) {
+    char *input = NULL;
+    size_t input_size = 0;
+    pid_t pid;
+    int status;
+
+    while (1) 
+    {
+        printf(PROMPT);
+        if (getline(&input, &input_size, stdin) == -1) 
+	{
+
+            printf("\n");
+            break;
+        }
+	input[strcspn(input, "\n")] = '\0'; 
+
+        pid = fork();
+        if (pid == -1) 
+	{
+            perror("Error");
+            exit(EXIT_FAILURE);
+        } else if (pid == 0) 
+	{
+            execute(input);
+            exit(EXIT_SUCCESS);
+        } 
+	else 
+	{
+            waitpid(pid, &status, 0);
+        }
+    }
+
+    free(input);
+    return (EXIT_SUCCESS);
+}
+
