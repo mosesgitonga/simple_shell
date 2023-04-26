48bb25f8e7c3ea7b8d30e38d3143952a541b892c
mosesgitonga <infosec947@gmail.com>
2023-04-26 14:58:05 +0300

    trial

--- main.h
+++ main.h
@@ -1,12 +1,13 @@
-#ifdef MAIN_H
+#ifndef MAIN_H
 #define MAIN_H
 
+#include <stdlib.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
 #include <sys/wait.h>
-
+int display();
 
 
 #endif
