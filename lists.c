eb80e49159c0f1a1e32b7088ee7315f6a16131fa
mosesgitonga <infosec947@gmail.com>
2023-04-28 22:20:22 +0300

    fin commit

--- lists.c
+++ lists.c
@@ -1,23 +1,12 @@
 #include "shell.h"
 
 /**
-<<<<<<< HEAD
-* add_node - adds a node to the start of the list
 * @head: address of pointer to head node
 * @str: str field of node
 * @num: node index used by history
 *
 * Return: size of list
 */
-=======
- * add_node - addd node sttart oof llist
- * @head: addr of node
- * @str: str field of node
- * @num:used by history
- *
- * Return: size of list
- */
->>>>>>> d8475d5e9d58efe967bf73b4014c9d6b255d20e3
 list_t *add_node(list_t **head, const char *str, int num)
 {
 	list_t *new_head;
@@ -54,15 +43,6 @@ list_t *add_node(list_t **head, const char *str, int num)
 *
 * Return: size of list
 */
-=======
- * add_node_end-adds node
- * @head: addr
- * @str: str
- * @num:node
- *
- * Return:siize
- */
->>>>>>> d8475d5e9d58efe967bf73b4014c9d6b255d20e3
 list_t *add_node_end(list_t **head, const char *str, int num)
 {
 	list_t *new_node, *node;
@@ -82,7 +62,6 @@ list_t *add_node_end(list_t **head, const char *str, int num)
 	new_node->str = str ? _strdup(str) : NULL;
 	if (!new_node->str && str)
 	{
-<<<<<<< HEAD
 		new_node->str = _strdup(str);
 
 		if (!new_node->str)
@@ -90,10 +69,8 @@ list_t *add_node_end(list_t **head, const char *str, int num)
 			free(new_node);
 			return (NULL);
 		}
-=======
 		free(new_node);
 		return (NULL);
->>>>>>> d8475d5e9d58efe967bf73b4014c9d6b255d20e3
 	}
 	if (node)
 	{
@@ -108,19 +85,10 @@ list_t *add_node_end(list_t **head, const char *str, int num)
 
 /**
 <<<<<<< HEAD
-* print_list_str - prints only the str element of 
-* a list_t linked list
-* @h: pointer to first node
-*
-* Return: size of list
-*/
-=======
- * print_list_str-prints string
- * @h:pointer first node
- *
+ *print_list_str -  printing string
+*@h:head
  * Return:list size
  */
->>>>>>> d8475d5e9d58efe967bf73b4014c9d6b255d20e3
 size_t print_list_str(const list_t *h)
 {
 	size_t i;
@@ -133,13 +101,6 @@ size_t print_list_str(const list_t *h)
 
 /**
 <<<<<<< HEAD
-* delete_node_at_index - A Function that deletes
-* a node at a specified index
-* @head: address of pointer to first node
-* @index: index of node to delete
-*
-* Return: 1 on success, 0 on failure
-*/
 =======
  * delete_node_at_index-del nodes
  * @head: addr of first node
@@ -147,7 +108,6 @@ size_t print_list_str(const list_t *h)
  *
  * Return:1
  */
->>>>>>> d8475d5e9d58efe967bf73b4014c9d6b255d20e3
 int delete_node_at_index(list_t **head, unsigned int index)
 {
 	list_t *node, *prev_node;
@@ -178,20 +138,12 @@ int delete_node_at_index(list_t **head, unsigned int index)
 	return (0);
 }
 /**
-<<<<<<< HEAD
-* free_list - A functin that  frees all
-* nodes of a list
-* @head_ptr: address of pointer to head node
-*
-* Return: void
-*/
 =======
  * free_list-frees list
  * @head_ptr: addr of ptr node
  *
  * Return: void
  */
->>>>>>> d8475d5e9d58efe967bf73b4014c9d6b255d20e3
 void free_list(list_t **head_ptr)
 {
 	list_t *node, *next_node, *head;
@@ -199,11 +151,8 @@ void free_list(list_t **head_ptr)
 	if (!head_ptr || !*head_ptr)
 	{
 		return;
-<<<<<<< HEAD
 	}
-=======
 
->>>>>>> d8475d5e9d58efe967bf73b4014c9d6b255d20e3
 	head = *head_ptr;
 	for (node = head; node; node = next_node)
 	{
