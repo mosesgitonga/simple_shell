3684a0fa52487612dcceb38aaded1ef4c3abc0ef
Wanjiruwanjiku-tech <natalieshiro2016@gmail.com>
2023-04-28 07:08:59 -0700

    Updates

--- memory.c
+++ memory.c
@@ -1,11 +1,12 @@
 #include "shell.h"
 
 /**
- * bfree - frees a pointer and NULLs the address
- * @ptr: address of the pointer to free
- *
- * Return: 1 if freed, otherwise 0.
- */
+*bfree - A Function that frees a pointer and
+*NULLs the address
+*@ptr: The address of the pointer to be freed
+*
+* Return: 1 if freed, otherwise 0.
+*/
 int bfree(void **ptr)
 {
 	if (ptr && *ptr)
--- realloc.c
+++ realloc.c
@@ -1,59 +1,76 @@
 #include "shell.h"
 
 /**
- **_memset - fills memory with a constant byte
- *@s: the pointer to the memory area
- *@b: the byte to fill *s with
- *@n: the amount of bytes to be filled
- *Return: (s) a pointer to the memory area s
- */
+**_memset -A Function that fills memory with
+*a constant byte
+*@s: The pointer to the memory area
+*@b: The byte to fill *s with
+*@n: The amount of bytes to be filled
+*Return: (s) a pointer to the memory area s
+*/
 char *_memset(char *s, char b, unsigned int n)
 {
-	unsigned int i;
+	unsigned int i; /*Loop Counter*/
 
-	for (i = 0; i < n; i++)
+	i = 0;
+
+	while (i < n)
+	{
 		s[i] = b;
+		i++;
+	}
 	return (s);
 }
 
 /**
- * ffree - frees a string of strings
- * @pp: string of strings
- */
+* ffree - A Function that frees a string of strings
+* @pp: The string of strings
+*/
 void ffree(char **pp)
 {
 	char **a = pp;
 
 	if (!pp)
+	{
 		return;
-	while (*pp)
+	}
+	for (; *pp; ++pp)
+	{
 		free(*pp++);
+	}
 	free(a);
 }
 
 /**
- * _realloc - reallocates a block of memory
- * @ptr: pointer to previous malloc'ated block
- * @old_size: byte size of previous block
- * @new_size: byte size of new block
- *
- * Return: pointer to da ol'block nameen.
- */
+* _realloc - A function that re-allocates 
+*a block of memory
+* @ptr: pointer to previous block
+* @old_size: byte size of previous block
+* @new_size: byte size of new block
+*
+* Return: pointer
+*/
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 {
 	char *p;
 
 	if (!ptr)
+	{
 		return (malloc(new_size));
+	}
 	if (!new_size)
+	{
 		return (free(ptr), NULL);
+	}
 	if (new_size == old_size)
+	{
 		return (ptr);
-
+	}
 	p = malloc(new_size);
 	if (!p)
+	{
 		return (NULL);
-
+	}
 	old_size = old_size < new_size ? old_size : new_size;
 	while (old_size--)
 		p[old_size] = ((char *)ptr)[old_size];
